4a02076bf7604299964ebd3bf8ec93404fc0c5ef
Fredora <fredora2014@gmail.com>
2022-12-09 05:03:43 -0800

    task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list and len(my_list):
-        num = 0
-        dem = 0
-        for tup in my_list:
-            num += (tup[0] * tup[1])
-            the += tup[1]
-        return (num / dem)
-    return 0
+    if len(my_list) == 0:
+        return (0)
+    sum = 0
+    div = 0
+    for item in my_list:
+        sum = sum + (item[0] * item[1])
+        div = div + item[-1]
+    return (sum/div)
 
